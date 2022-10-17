d13529af7e144a9355839c1ff9dc9be8447902f2
nookworth <nooksack@gmail.com>
2022-10-16 19:09:16 -0600

    Model relationships defined.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,27 +1,41 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
+const mysql = require("mysql2");
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // MySQL password
+    password: process.env.DB_PASSWORD,
+    database: "ecommerce_db",
+  },
+  console.log(`Connected to the ecommerce_db database.`)
+);
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  const categories = Category.findAll();
+  res.json(categories);
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
 });
 
